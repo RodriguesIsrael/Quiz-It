6b08fea0017438538f12e8eb0be1cc11adb67f88
RodriguesIsrael <zbinda956@gmail.com>
2023-10-18 15:28:10 +0000

    commit the app.js sheet

--- assents/js/end.js
+++ assents/js/end.js
@@ -12,70 +12,70 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
 
-  saveScoreBtn.disabled = !username.value;
+    saveScoreBtn.disabled = !username.value;
 });
 
 let saveHighScore = e => {
-  console.log("clicked the save button!");
-  e.preventDefault();
+    console.log("clicked the save button!");
+    e.preventDefault();
 
-  const score = {
-    score: Math.floor(Math.random() * 100),
-    name: username.value
-  };
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
 
-  highScores.push(score);
-  highScores.sort((a, b) => b.score - a.score);
-  highScores.splice(5);
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-  localStorage.setItem('highScores', JSON.stringify(highScores));
-  window.location.assign("/");
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("/");
 
-  console.log(highScores);
+    console.log(highScores);
 
 };
 
 stars[0].onclick = function () {
-  stars[0].style.color = "#ffd93b";
-  stars[1].style.color = "#e4e4e4";
-  stars[2].style.color = "#e4e4e4";
-  stars[3].style.color = "#e4e4e4";
-  stars[4].style.color = "#e4e4e4";
-  emoji.style.transform = "translateX(0)";
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#e4e4e4";
+    stars[2].style.color = "#e4e4e4";
+    stars[3].style.color = "#e4e4e4";
+    stars[4].style.color = "#e4e4e4";
+    emoji.style.transform = "translateX(0)";
 }
 
 stars[1].onclick = function () {
-  stars[0].style.color = "#ffd93b";
-  stars[1].style.color = "#ffd93b";
-  stars[2].style.color = "#e4e4e4";
-  stars[3].style.color = "#e4e4e4";
-  stars[4].style.color = "#e4e4e4";
-  emoji.style.transform = "translateX(-80px)";
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#e4e4e4";
+    stars[3].style.color = "#e4e4e4";
+    stars[4].style.color = "#e4e4e4";
+    emoji.style.transform = "translateX(-80px)";
 };
 
 stars[2].onclick = function () {
-  stars[0].style.color = "#ffd93b";
-  stars[1].style.color = "#ffd93b";
-  stars[2].style.color = "#ffd93b";
-  stars[3].style.color = "#e4e4e4";
-  stars[4].style.color = "#e4e4e4";
-  emoji.style.transform = "translateX(-160px)";
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#ffd93b";
+    stars[3].style.color = "#e4e4e4";
+    stars[4].style.color = "#e4e4e4";
+    emoji.style.transform = "translateX(-160px)";
 };
 
 stars[3].onclick = function () {
-  stars[0].style.color = "#ffd93b";
-  stars[1].style.color = "#ffd93b";
-  stars[2].style.color = "#ffd93b";
-  stars[3].style.color = "#ffd93b";
-  stars[4].style.color = "#e4e4e4";
-  emoji.style.transform = "translateX(-240px)";
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#ffd93b";
+    stars[3].style.color = "#ffd93b";
+    stars[4].style.color = "#e4e4e4";
+    emoji.style.transform = "translateX(-240px)";
 };
 
 stars[4].onclick = function () {
-  stars[0].style.color = "#ffd93b";
-  stars[1].style.color = "#ffd93b";
-  stars[2].style.color = "#ffd93b";
-  stars[3].style.color = "#ffd93b";
-  stars[4].style.color = "#ffd93b";
-  emoji.style.transform = "translateX(-320px)";
+    stars[0].style.color = "#ffd93b";
+    stars[1].style.color = "#ffd93b";
+    stars[2].style.color = "#ffd93b";
+    stars[3].style.color = "#ffd93b";
+    stars[4].style.color = "#ffd93b";
+    emoji.style.transform = "translateX(-320px)";
 };
