339573368325167080abeb274b46fcfb1b1f4fad
RodriguesIsrael <zbinda956@gmail.com>
2023-10-20 11:13:45 +0000

    change the random fot mostRecentScore

--- assets/js/end.js
+++ assets/js/end.js
@@ -20,7 +20,7 @@ let saveHighScore = e => {
     e.preventDefault();
 
     const score = {
-        score: Math.floor(Math.random() * 100),
+        score: mostRecentScore, //Math.floor(Math.random() * 100),
         name: username.value
     };
 
